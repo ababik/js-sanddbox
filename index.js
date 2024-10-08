a6f3be60fa250969e5ed7472ce75d44a483b13ef
Alexey Babik <alexey.babik@gmail.com>
2024-10-08 15:50:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
     const script = document.createElement("script")
     script.setAttribute("src", "dependency.js")
     script.setAttribute("async", "true")
     document.body.appendChild(script)
-})
-
