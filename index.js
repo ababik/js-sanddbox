40dd1c533839813cba257dad183ded46dd64d438
Alexey Babik <alexey.babik@gmail.com>
2024-10-08 15:45:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-document.addEventListener("readystatechange", () => {
-    console.log("readystatechange", document.readyState)
-})
-window.addEventListener("load", () => {
-    console.log("load")
-})
 window.addEventListener("DOMContentLoaded", () => {
-    console.log("DOMContentLoaded")
     const script = document.createElement("script")
     script.setAttribute("src", "dependency.js")
     script.setAttribute("async", "true")
